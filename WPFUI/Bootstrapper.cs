d881d5f61101d7334fdf4601a1af25060b547e9c
Dee Vagholkar <devashri.vagholkar@ansys.com>
2020-12-04 08:55:19 -0500

    added folder WPFUI

---
+++ WPFUI/Bootstrapper.cs
@@ -0,0 +1,26 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WPFUI.ViewModels;
+
+namespace WPFUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        //there is a OnStartUp method in caliburn Bootstrapper, but we want to override it
+        //thats why add keyword override to your own OnStartup method as shown below
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
---
+++ WPFUI/ViewModels/ShellViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFUI.ViewModels
+{
+    //screen allows prompts before closing - "do you want to save?" "are you sure"
+    public class ShellViewModel : Screen
+    {
+        private string _firstName = "Tim" ;
+
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                _firstName = value;
+            }
+        }
+
+    }
+}
