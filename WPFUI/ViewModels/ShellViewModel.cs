5fb2976ca0eaf0d52c8dbb6be8e36a08d32b788b
Dee Vagholkar <devashri.vagholkar@ansys.com>
2020-12-21 13:46:07 -0500

    completed tutorial 3 of Tim Corey

--- WPFUI/ViewModels/ShellViewModel.cs
+++ WPFUI/ViewModels/ShellViewModel.cs
@@ -4,13 +4,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WPFUI.Models;
 
 namespace WPFUI.ViewModels
 {
     //screen allows prompts before closing - "do you want to save?" "are you sure"
-    public class ShellViewModel : Screen
+    public class ShellViewModel : Conductor<object>
     {
         private string _firstName = "Tim" ;
+        private string _lastName;
+        private BindableCollection<PersonModel> _people = new BindableCollection<PersonModel>();
+        private PersonModel _selectedPerson;
+
+        public ShellViewModel()
+        {
+            People.Add(new PersonModel { FirstName = "Tim", LastName = "Corey" });
+            People.Add(new PersonModel { FirstName = "Jim", LastName = "Smith" });
+            People.Add(new PersonModel { FirstName = "Ray", LastName = "George" });
+
+        }
 
         public string FirstName
         {
@@ -21,8 +33,73 @@ namespace WPFUI.ViewModels
             set
             {
                 _firstName = value;
+                NotifyOfPropertyChange(()=>FirstName); //updates value everywhere if changed
+                NotifyOfPropertyChange(() => FullName);
+
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+                NotifyOfPropertyChange(() => LastName);
+                NotifyOfPropertyChange(() => FullName);
+
+            }
+        }
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
             }
         }
 
+
+        public BindableCollection<PersonModel> People
+        {
+            get { return _people; }
+            set { _people = value; }
+        }
+
+        public PersonModel SelectedPerson
+        {
+            get { return _selectedPerson; }
+            set
+            {
+                _selectedPerson = value;
+                NotifyOfPropertyChange(() => SelectedPerson);
+            }
+        }
+
+        public bool CanClearText(string firstName, string lastName)
+        {
+            if (string.IsNullOrWhiteSpace(firstName) && string.IsNullOrWhiteSpace(lastName))
+            {
+                return false;
+            }
+            else
+                return true;
+        }
+        public void ClearText(string firstName, string lastName)
+        {
+            FirstName = "";
+            LastName = "";
+        }
+
+        public void LoadPageOne()
+        {
+            ActivateItem(new FirstChildViewModel());
+        }
+
+        public void LoadPageTwo()
+        {
+            ActivateItem(new SecondChildViewModel());
+        }
     }
 }
